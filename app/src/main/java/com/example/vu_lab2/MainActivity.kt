0e4915ff8a99deaa80d350ae36cfeae710f790f0
Lukas A <lukas.aleksa@agmis.lt>
2022-03-10 15:33:26 +0200

    Updated base code and added additional words to strings.xml

--- app/src/main/java/com/example/vu_lab2/MainActivity.kt
+++ app/src/main/java/com/example/vu_lab2/MainActivity.kt
@@ -1,18 +1,13 @@
 package com.example.vu_lab2
 
-import android.os.Build
 import android.os.Bundle
-import android.text.Editable
-import android.util.Log
 import android.view.View
 import android.widget.AdapterView
 import android.widget.Toast
-import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -36,17 +31,15 @@ class MainActivity : AppCompatActivity() {
 
         button_count.setOnClickListener {
             when {
-                selectedSpinnerChoice == "Count words" -> {
+                selectedSpinnerChoice == String.format(getString(R.string.spinner_count_words)) -> {
                     counterValue = CountingActivity().wordCount(textEditorValue.toString())
-//                    Toast.makeText(this, getString(R.string.word_count_toast, counterValue),Toast.LENGTH_SHORT ).show()
                     counter_text.text = String.format(getString(R.string.word_count_toast, counterValue))
                 }
                 textEditorValue.isEmpty() || textEditorValue.isNullOrBlank()-> {
                     Toast.makeText(this, R.string.edit_text_empty_toast, Toast.LENGTH_SHORT).show()
                 }
-                selectedSpinnerChoice == "Count punctuation symbols" -> {
+                selectedSpinnerChoice == String.format(getString(R.string.spinner_count_punctuation)) -> {
                     counterValue = CountingActivity().punctuationCount(textEditorValue.toString())
-//                    Toast.makeText( this, getString(R.string.word_punct_toast, counterValue),Toast.LENGTH_SHORT).show()
                     counter_text.text = String.format(getString(R.string.word_punct_toast, counterValue))
                 }
                 else -> {
